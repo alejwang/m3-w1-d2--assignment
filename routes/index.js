d86fd01dfc484ae8d99e6991bf25cf44b92b8b79
Zander Wang <me@zander.wang>
2025-05-15 21:28:13 -0700

    finish the week 2 lab

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
+const path = require('path');
+const auth = require('http-auth');
+
 const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
 const { check, validationResult } = require('express-validator');
+
+const router = express.Router();
+const Registration = mongoose.model('Registration');
+
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd'),
+})
 
 router.get('/', (req, res) => {
     res.render('form', { title: 'Sign up form' });
@@ -20,7 +30,13 @@ router.post('/',
         const errors = validationResult(req);
         // console.log(errors, errors.isEmpty());
         if (errors.isEmpty()) {
-            res.send('Thank you for your registration!')
+            const registration = new Registration(req.body);
+            registration.save()
+                .then(() => { res.send('Thank you for your registration!') })
+                .catch((err) => {
+                    console.log(err);
+                    res.send('Sorry! Something went wrong');
+                }) 
         } else {
             res.render('form', {
                 title: 'Sign up form',
@@ -30,5 +46,15 @@ router.post('/',
         }
     }
 );
+
+
+router.get('/registrations', basic.check((req, res) => {
+    Registration.find()
+        .then((registrations) => {
+            res.render('index', { title: 'Listing registrations', registrations});
+        })
+        .catch(() => { res.send('Sorry! Something went wrong.')})
+    // res.render('index', { title: 'Listing registrations' });
+}));
 
 module.exports = router;
--- start.js
+++ start.js
@@ -1,3 +1,19 @@
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE);
+
+mongoose.connection
+    .on('open', () => {
+        console.log('Mongoose connection open');
+    })
+    .on('error', (err) => {
+        console.log(`Connection error: ${err.message}`)
+    });
+
+require('./models/Registration');
+
 const app = require('./app');
 const port = 3000;
 
